14502c2b9af14f4eabf529c564380db879032903
Kevin Liu <kevinliu@princeton.edu>
2016-05-08 04:15:52 -0400

    final processing

--- impute.m
+++ impute.m
@@ -1,4 +1,4 @@
 data = csvread('data_super_nan.csv',1,1);
 data_imputed = knnimpute(data');
 data_imputed = data_imputed';
-csvwrite('data_imputed.csv',data_imputed)
+dlmwrite('data_imputed.csv',data_imputed,'precision','%6.6f')
